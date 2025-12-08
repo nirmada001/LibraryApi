ef5f955cb5032c70e13bc09d09b9f7fbb0a12107
Nirmada Edirisinghe <134194456+nirmada001@users.noreply.github.com>
2025-12-08 18:23:31 +0530

    Book List styling using Tailwind Css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { createBook } from './api/booksapi'
 import { updateBook } from './api/booksapi'
 import { deleteBook } from './api/booksapi'
 
+
 function App() {
 
   const [books, setBooks] = useState<Book[]>([]);
@@ -105,29 +106,29 @@ function App() {
 
   return (
     <>
-    <div className='min-h-screen bg-gray-100 p-6'>
-      <div className='max-w-5xl mx-auto bg-white rounded-lg shadow p-6'>
-        <h1 className='text-2xl font-bold text-gray-800 mb-4'> Library Management System </h1>
-        <div className='flex gap-6'>
-          {/* Form Section */}
-          <div className='w-1/3 bg-gray-50 border border-gray-200 rounded-lg p-4'>
-            <h2 className='text-lg font-semibold text-gray-800 mb-3'>
-              {editingId ? "Edit Book" : "Add New Book"}
-            </h2>
-            <form onSubmit={handleSubmit} className='space-y-4'>
-              <input 
-                type="text"
-                placeholder='Title'
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className='w-full border border-gray-300 rounded p-2 text-sm' />
+      <div className='min-h-screen bg-gray-100 p-6'>
+        <div className='max-w-5xl mx-auto bg-white rounded-lg shadow p-6'>
+          <h1 className='text-2xl font-bold text-gray-800 mb-4'> Library Management System </h1>
+          <div className='flex gap-6'>
+            {/* Form Section */}
+            <div className='w-1/3 bg-gray-50 border border-gray-200 rounded-lg p-4'>
+              <h2 className='text-lg font-semibold text-gray-800 mb-3'>
+                {editingId ? "Edit Book" : "Add New Book"}
+              </h2>
+              <form onSubmit={handleSubmit} className='space-y-4'>
+                <input
+                  type="text"
+                  placeholder='Title'
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className='w-full border border-gray-300 rounded p-2 text-sm' />
 
-                <input 
-                type="text"
-                placeholder='Author'
-                value={author}
-                onChange={(e) => setAuthor(e.target.value)}
-                className='w-full border border-gray-300 rounded p-2 text-sm' />
+                <input
+                  type="text"
+                  placeholder='Author'
+                  value={author}
+                  onChange={(e) => setAuthor(e.target.value)}
+                  className='w-full border border-gray-300 rounded p-2 text-sm' />
 
                 <textarea
                   placeholder='Description'
@@ -138,12 +139,46 @@ function App() {
                 <button type="submit" className='w-full bg-blue-500 text-white rounded p-2 text-sm'>
                   {editingId ? "Update Book" : "Add Book"}
                 </button>
-            </form>
+              </form>
+            </div>
+            {/* Book List Section */}
+            <div className='w-2/3 space-y-4'>
+              <h2 className='text-lg font-semibold text-gray-800 mb-3'>Book List</h2>
+              <ul className='space-y-3'>
+                {books.map(book => (
+                  <li
+                    key={book.id}
+                    className='border border-gray-200 rounded-lg p-4 bg-white shadow-sm'
+                  >
+                    <p className='text-xs text-gray-500'>ID: {book.id}</p>
+                    <h3 className='text-md font-bold text-gray-900'>Title: {book.title}</h3>
+                    <p className='text-sm text-gray-600'>Author: {book.author}</p>
+                    <p className='text-sm text-gray-700 mt-2'>Description: {book.description}</p>
+
+                    <div className='flex gap-2 mt-2'>
+                      <button
+                        onClick={() => {
+                          setEditingId(book.id);
+                          setTitle(book.title);
+                          setAuthor(book.author);
+                          setDescription(book.description);
+                        }}
+                        className='px-3 py-1 text-xs bg-yellow-200 text-yellow-800 rounded'> Edit</button>
+                      <button
+                        onClick={() => handleDeleteBook(book.id)}
+                        className="px-3 py-1 text-xs bg-red-200 text-red-800 rounded"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  </li>
+
+                ))}
+              </ul>
+            </div>
           </div>
-          {/* Book List Section */}
         </div>
       </div>
-    </div>
     </>
   )
 }
