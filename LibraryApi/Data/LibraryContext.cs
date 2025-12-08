6dbe96333b42e67b746c962cc6bc03eddbbc113d
Nirmada Edirisinghe <134194456+nirmada001@users.noreply.github.com>
2025-12-08 10:36:14 +0530

    Add project files.

---
+++ LibraryApi/Data/LibraryContext.cs
@@ -0,0 +1,12 @@
+﻿using LibraryApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryApi.Data
+{
+    public class LibraryContext : DbContext
+    {
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ LibraryApi/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibraryApi.Models
+{
+    public class Book
+    {
+
+        public int Id { get; set; }
+      
+        public string Title { get; set; }
+       
+        public string Author { get; set; }
+
+        public string Description { get; set; }
+
+
+    }
+}
