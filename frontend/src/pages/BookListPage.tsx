fd043e218122138eab5cf3578ca23dbe6f19587a
Nirmada Edirisinghe <134194456+nirmada001@users.noreply.github.com>
2025-12-09 15:42:21 +0530

    UI enhancements

--- frontend/src/pages/BookListPage.tsx
+++ frontend/src/pages/BookListPage.tsx
@@ -46,7 +46,7 @@ export default function BookListPage() {
     return (
         <div className='max-w-3xl m-auto p-6 rounded shadow-md min-h-[60vh] mt-5'>
             <div className='flex items-center justify-between mb-4'>
-                <h2 className='text-lg font-semibold text-gray-800'>Book List</h2>
+                <h2 className='text-2xl font-semibold text-gray-800'>Book List</h2>
                 <div className='space-x-2'>
                     <Link to="/" className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Home</Link>
                     <Link to="/add-book" className='px-4 py-2 bg-green-500 text-white rounded hover:bg-green-700'>Add New Book</Link>
@@ -68,26 +68,29 @@ export default function BookListPage() {
                 </div>
             )}
 
-            {books.length === 0 ?(
-                <p className='text-sm text-gray-500'>No books available. Click "Add New Book" to add books.</p>
-            ):(
-                <ul className='space-y-3'>
-                    {books.map((book) =>(
-                        <li key={book.id} className='w-2/4 border border-gay-200 rounded-lg p-4 bg-white shadow-sm flex justify-between items-start'>
-                            <div>
-                                <p className='text-sm text-gray-500'>ID: {book.id}</p>
-                                <h3 className='text-lg font-semibold text-gray-800'>{book.title}</h3>
-                                <p className='text-sm text-gray-600'>Author: {book.author}</p>
-                                <p className='text-sm text-gray-600'>Description: {book.description}</p>
-                            </div>
-                            <div className='flex flex-col gap-2 items-end'>
-                                <button onClick={() => navigate(`/edit-book/${book.id}`)} className='px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-700'>Edit</button>
-                                <button onClick={() => handleDelete(book.id)} className='px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700'>Delete</button>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            )}
+            
+            <div className='flex justify-center'>
+                {books.length === 0 ? (
+                    <p className='text-sm text-gray-500'>No books available. Click "Add New Book" to add books.</p>
+                ) : (
+                    <ul className='space-y-4 w-full max-w-lg'>
+                        {books.map((book) => (
+                            <li key={book.id} className='border border-gay-200 rounded-lg p-4 bg-white shadow-sm flex justify-between items-start'>
+                                <div>
+                                    <p className='text-sm text-gray-500'>ID: {book.id}</p>
+                                    <h3 className='text-lg font-semibold text-gray-800'>{book.title}</h3>
+                                    <p className='text-sm text-gray-600'>Author: {book.author}</p>
+                                    <p className='text-sm text-gray-600'>Description: {book.description}</p>
+                                </div>
+                                <div className='flex flex-col gap-2 items-end'>
+                                    <button onClick={() => navigate(`/edit-book/${book.id}`)} className='flex-1 px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-700'>Edit</button>
+                                    <button onClick={() => handleDelete(book.id)} className='flex-1 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700'>Delete</button>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                )}
+            </div>
         </div>
     )
 }
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -3,14 +3,23 @@ import { Link } from 'react-router-dom'
 
 export default function HomePage() {
   return (
-    <div className="min-h-[60vh] flex items-center justify-center">
-        <div className='text-center space-y-4 rounded p-8 bg-white shadow-md'>
-            <h1 className='text-2xl font-bold text-gray-800'>Library Management System</h1>
-            <p className='text-smtest-gray-600'>Choose an action below...</p>
+    <div className="min-h-[60vh] flex flex-col items-center justify-center">
+      <h1 className='text-4xl font-bold text-gray-800 mt-10'>Welcome to the Library Management System</h1>
+        <div className='w-1/3 text-center space-y-4 rounded p-8 bg-gray-100 shadow-md mt-8'>
+        <h1 className='text-xl font-bold text-gray-800 mt-10'>Library Management System</h1>
+            <p className='text-sm text-gray-600'>Choose an action below...</p>
             <div className='flex justify-center gap-4 mt-4'>
                 <Link to="/add-book" className='px-6 py-3 bg-blue-500 text-white rounded hover:bg-blue-700'>Add New Book</Link>
                 <Link to="/books" className='px-6 py-3 bg-green-500 text-white rounded hover:bg-green-700'>View Books</Link>
             </div>
+        </div>
+        {/* About Section */}
+        <div className='mt-10 max-w-3xl mx-auto p-6 bg-green-100 border-green-200 rounded shadow-md'>
+            <h2 className='text-xl font-semibold mb-4 text-gray-800'>About This System</h2>
+            <p className='text-gray-600'>
+                This Library Management System allows users to efficiently manage a collection of books. 
+                Users can add new books, view the list of available books, edit book details, and delete books from the collection.
+            </p>
         </div>
     </div>
   )
